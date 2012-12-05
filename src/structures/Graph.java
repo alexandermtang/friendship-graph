2a9f446a9ad47b3efbcacc17abc3512f9d9005df
Alex Tang <alex.m.tang@gmail.com>
2012-12-04 23:22:31 -0500

    last commit before rewrite

--- src/structures/Graph.java
+++ src/structures/Graph.java
@@ -17,7 +17,7 @@ public class Graph {
 	// nameHash used to translate from person's name to vertex number
 	HashMap<String,Integer> nameHash;
 	
-	// key is name of school, value is linkedlist of all students in that school
+	// key is name of school, value is a LinkedList of all students in that school
 	HashMap<String,LinkedList<Integer>> studentsInSchool = new HashMap<String,LinkedList<Integer>>();
 	
 	// Initialize graph with input from file, builds adjacencyLL
@@ -25,8 +25,7 @@ public class Graph {
 		sc.useDelimiter("\n");
 		int size = sc.nextInt();
 		adjlists = new Vertex[size];
-		nameHash = new HashMap<String,Integer>(size/10,1f);
-		// may need to change initial capacity of nameHash!!!!!!!!!!
+		nameHash = new HashMap<String,Integer>();
 		
 		// read vertices
 		for(int v = 0; v < adjlists.length; v++) {	
@@ -62,12 +61,9 @@ public class Graph {
 		
 		
 		// used to iterate through studentsInSchool
-	/*	Iterator it = studentsInSchool.entrySet().iterator();
-		while(it.hasNext()) {
-			Map.Entry pairs = (Map.Entry)it.next();
-			System.out.println(pairs.getKey() + " = " + pairs.getValue());
-			it.remove();
-		}*/
+		for(Map.Entry entry : studentsInSchool.entrySet()) {
+			System.out.println(entry.getKey() + " = " + entry.getValue());
+		}
 	}	
 	
 	// returns vnum of name using nameHash, -1 if not present
@@ -76,12 +72,49 @@ public class Graph {
 	// Uses BFS to print out an adjlists of subgraph of students in school
 	public Graph subgraph(String school) {
 		Queue<Integer> q = new LinkedList<Integer>();
-		boolean[] visited = new boolean[adjlists.length];
+		Vertex[] schooladjlists = new Vertex[studentsInSchool.get(school).size()];
+		boolean[] visited = new boolean[schooladjlists.length];
 		Arrays.fill(visited, false);
-		Graph g = new Graph();
+
+		HashMap<Integer,Integer> previous = new HashMap<Integer,Integer>();
+		
+		LinkedList<Integer> tovisit = studentsInSchool.get(school);
+		for(int i=0; i < tovisit.size(); i++) {
+			schooladjlists[i] = new Vertex(adjlists[tovisit.get(i)].name, school, null);
+			
+			
+			
+			
+			
+		}
+		int start = studentsInSchool.get(school).get(0);
+		int curr = start;
+		int finish;
+		
+		previous.put(curr,start);
+		q.add(curr);
+		visited[curr] = true;
+		
+		while(!q.isEmpty()) {
+			curr = (int)q.remove(); 
+
+			for(Neighbor ptr = adjlists[curr].neighbors; ptr != null; ptr = ptr.next) {
+				if(!visited[ptr.vnum]) {
+					q.add(ptr.vnum);
+					visited[ptr.vnum] = true;
+					previous.put(ptr.vnum,curr);
+				}
+			}
+			finish = curr;
+		}
+		
+		
+		ArrayList<String> reversepath = new ArrayList<String>();
+		reversepath.add(adjlists[finish].name);
+		for(int i=finish; i != start; i=previous.get(i)) { 
+			reversepath.add(adjlists[previous.get(i)].name); 
+		}
 	
-		
-		
 		/* FINISH THIS SHIT NIGGAAAAAAAAAAAAAAAAAA */
 		visited[0] = true;
 		if(adjlists[0].school.equals(school)) {
@@ -92,20 +125,17 @@ public class Graph {
 			int w = q.remove();
 		}
 		
+		Graph g = new Graph();
+		g.adjlists = schooladjlists;
 		return g;
 	}
 	
 	public ArrayList<Graph> cliques() {
 		ArrayList<Graph> cliques = new ArrayList<Graph>();
-		
-		Graph g = new Graph();
-		
+
 		// used to iterate through studentsInSchool
-		Iterator it = studentsInSchool.entrySet().iterator();
-		while(it.hasNext()) {
-			Map.Entry pairs = (Map.Entry)it.next();
-			cliques.add(subgraph((String)pairs.getKey()));
-			it.remove();
+		for(String school : studentsInSchool.keySet()) {
+			cliques.add(subgraph(school));
 		}
 		
 		return cliques; 
